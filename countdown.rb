2e347e78620a2ae53ae3183ef9721371472ab74d
Bria White <bria.awhite@gmail.com>
2020-08-02 21:49:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
-#write your code here
 
-def countdown
+def countdown(countdown_output)
+  countdown_output = 10
+  countdown = 12
+
+  while countdown_output > 0
+   if countdown_output == countdown
+     puts "HAPPY NEW YEAR!"
+    countdown_output += 2 
+      puts "#{countdown_output} SECOND(S)!"
+    countdown_output -= 1
+  
+ end
 end
+
