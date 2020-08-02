9ae70abffe8c8443e0ea3cd94c2b0e48e5a2cf92
Bria White <bria.awhite@gmail.com>
2020-08-02 21:55:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,12 +4,10 @@ def countdown(countdown_output)
   countdown = 12
 
   while countdown_output > 0
-   if 
-     puts "HAPPY NEW YEAR!"
-    c 
-      puts "#{countdown_output} SECOND(S)!"
-    countdown_output -= 1
+   puts "#{countdown_output} SECOND(S)!"
+  countdown_output -= 1
   
  end
+ "HAPPY NEW YEAR"
 end
 
