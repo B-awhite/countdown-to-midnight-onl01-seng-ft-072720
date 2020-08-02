cc679cf7d18552c4f28a4070cec8dea50aac6f43
Bria White <bria.awhite@gmail.com>
2020-08-02 22:14:42 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
-#write your code here
+countdown_output = 10
+countdown = 12 
 
-def countdown
+def countdown(countdown_output)
+  
+
+  while countdown_output > 0
+   puts "#{countdown_output} SECOND(S)!"
+  countdown_output -= 1
+  
+ end
+ "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(countdown_output)
+  
+  while countdown_output > 0
+   puts "#{countdown_output} SECOND(S)"
+   sleep(1)
+  countdown_output -= 1
+ end
+ "HAPPY NEW YEAR!"
 end
